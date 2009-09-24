203521289f7a0e5b5000305ceae571212d6228dc
shannonastolfi <shannonastolfi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-24 00:11:20 +0000

    CID admin display
    git-svn-id: https://plugins.svn.wordpress.org/scriblio-schema-cid/trunk@157411 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- editor.js
+++ editor.js
@@ -1,34 +1,33 @@
 function cid_meditor() {
+	jQuery("div.wrap h2:first").text("Organization Information");
+
 	jQuery("#screen-meta").hide();
 	jQuery("#screen-options-link-wrap").hide();
 
 	jQuery("#bsuite_machinetags").hide();
+	jQuery("#bsuite_post_icon").hide();
 	jQuery("#categorydiv").hide();
 	jQuery("#tagsdiv").hide();
 	jQuery("#trackbacksdiv").hide();
 	jQuery("#postcustom").hide();
 	jQuery("#passworddiv").hide();
-
-	jQuery("div.wrap h2:first").text("Business Information");
+	jQuery("#postexcerpt").hide();
+	
 
 	jQuery("#titlediv #title").css({ marginLeft:"5px", width:"98%" });
 	jQuery("#titlediv").addClass("postbox");
-	jQuery("#titlediv #titlewrap").before('<h3 class="hndle"><label for="title">Business Name</label></h3>');
+	jQuery("#titlediv #titlewrap").before('<h3 class="hndle"><label for="title">Organization Name</label></h3>');
 
-	jQuery("#postdivrich #editor-toolbar #media-buttons").prepend('Long Description &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;');
+	jQuery("#scrib_meditor_div").css({ display:"block" , overflow:"auto"});
+	jQuery("#scrib_meditor_div h3").before('<h2>Details</h2>');
+	jQuery("#scrib_meditor_div").removeClass('closed');
 
-	jQuery("#postexcerpt").css({ display:"block"});
+        jQuery("#postdivrich #editor-toolbar").before('<h2>Short Description</h2>');
+	
+	
+	/*jQuery("#postexcerpt").css({ display:"block"});
 	jQuery("#postexcerpt h3").text("Short Description");
 	jQuery("#postexcerpt").removeClass('closed');
-	jQuery("#postexcerpt").insertAfter("#titlediv");
+	
 
-	jQuery("#scrib_meditor_div").css({ display:"block"});
-	jQuery("#scrib_meditor_div h3").text("Details");
-	jQuery("#scrib_meditor_div").removeClass('closed');
-	jQuery("#scrib_meditor_div").insertAfter("#titlediv");
-
-	jQuery("#bsuite_post_icon").css({ display:"block"});
-	jQuery("#bsuite_post_icon h3").text("Logo or Photograph");
-	jQuery("#bsuite_post_icon").removeClass('closed');
-	jQuery("#bsuite_post_icon").insertAfter('#titlediv');
 }
